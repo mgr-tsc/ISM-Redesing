5d02f09a5deaf170ee755bc2308b71196dfd4d3f
marcoguevararodriguez <marco2029guerod@outlook.com>
2023-11-17 15:39:39 -0500

    Migrations folder to Sqlite  added

---
+++ Migrations/Sqlite/20231117202424_TestingSqlite.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ISMRedesing.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class TestingSqlite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Expenses");
+        }
+    }
+}
